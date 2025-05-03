2d5d6d62bc227c5c2da66db9c037e763082aa971
c00302990 <c00302990@setu.ie>
2025-05-04 00:11:59 +0900

    test: Render 응답에 CORS 헤더가 유지되는지 확인하기 위한 필터 추가

---
+++ backend/src/main/java/com/changmin/securewebapp/config/TestCorsForceHeaderFilter.java
@@ -0,0 +1,23 @@
+package com.changmin.securewebapp.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class TestCorsForceHeaderFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE OPTIONS");
+        res.setHeader("Access-Control-Allow-Headers","*");
+
+        chain.doFilter(request, response);
+    }
+}
