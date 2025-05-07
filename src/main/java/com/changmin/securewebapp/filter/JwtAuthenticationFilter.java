c29205c4739495e6c9d00edd9ad1b6afcbd150c0
c00302990 <c00302990@setu.ie>
2025-05-07 21:45:43 +0900

    refactor: JwtAuthenticationFilter 빈 등록 방식 변경(@Component -> @Bean)

--- src/main/java/com/changmin/securewebapp/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/changmin/securewebapp/filter/JwtAuthenticationFilter.java
@@ -13,25 +13,17 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
 
-@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private final JwtUtil jwtUtil;
 	private final UserRepository userRepository;
 	private final LogoutService logoutService;
-
-	public JwtAuthenticationFilter(JwtUtil jwtUtil, LogoutService logoutService, UserRepository userRepository){
-		this.jwtUtil = jwtUtil;
-		this.userRepository = userRepository;
-		this.logoutService = logoutService;
-	}
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request,
