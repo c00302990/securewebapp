d7d6ecba34d846075e901428b488d17b60e1fd6c
c00302990 <c00302990@setu.ie>
2025-05-13 16:59:11 +0900

    fix: 전역 예외 처리에 모든 예외 로그 출력 추가

--- src/main/java/com/changmin/securewebapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/changmin/securewebapp/exception/GlobalExceptionHandler.java
@@ -8,13 +8,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex){
+        ex.printStackTrace();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotException(ResourceNotFoundException ex){
+        ex.printStackTrace();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleOtherExceptions(Exception ex){
+        ex.printStackTrace();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("서버 오류 발생: " + ex.getMessage());
     }
 }
